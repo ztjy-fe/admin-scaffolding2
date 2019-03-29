40914792610f197fb5dc69f369cdd15407e159b5
zhangzhenglong <zhangzhenglong@szy.cn>
2019-03-29 14:50:55 +0800

    update

--- meta.js
+++ meta.js
@@ -24,20 +24,7 @@ module.exports = {
 		"author": {
 			"type": "string",
 			"message": "作者"
-		},
-		"VueLazyload":{
-			"type": "confirm",
-			"message": "是否安装图片延迟加载插件VueLazyload？"
-		},
-		"tabs":{
-			"type": "confirm",
-			"message": "是否采用tabs形式加载路由组件？"
 		}
 	},
-	"filters": {
-		"src/assets/images/lazyload/*": "VueLazyload",
-		"src/store/modules/tabs.js": "tabs",
-		"src/views/layout/components/Tabs.vue": "tabs"
-	},
-	"completeMessage": "请按步骤运行以下命令:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\n文档说明见https://github.com/ztjy-fe/admin-scaffolding"
+	"completeMessage": "请按步骤运行以下命令:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\n文档说明见https://github.com/ztjy-fe/admin-scaffolding2"
 };
